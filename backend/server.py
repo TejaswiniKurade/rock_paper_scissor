7807a5e800ad1a1f3adc6cd759db494a150368fc
TejaswiniKurade <tejaswinikurade2503@gmail.com>
2024-02-29 12:03:06 +0530

    Changes done

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_socketio import emit, join_room
 
+
+current_game_id = 0  # Our simple counter
+
+def generate_game_id():
+    global current_game_id
+    current_game_id += 1
+    return current_game_id 
+
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/static') 
 app.config['SECRET_KEY'] = 'my_key' 
 socketio = SocketIO(app)
@@ -10,9 +18,26 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
 
+game_rooms = {}  
 
+@socketio.on('join-room')
+def handle_join_room(data):
+    room_id = data['roomId']
 
+    # Create the room if it doesn't exist
+    if room_id not in game_rooms:
+        game_rooms[room_id] = {
+            'players': [], 
+            'moves': {}
+        }
+
+    # Add the player to the room  
+    game_rooms[room_id]['players'].append({'id': socketio.sid})
+
+    join_room(room_id)
 
 
 
@@ -23,24 +48,7 @@ def index():
 
 # if __name__ == '__main__':
 #     socketio.run(app, debug=True)
-# game_rooms = {}  # Our main dictionary to store rooms
-
-# @socketio.on('join-room')
-# def handle_join_room(data):
-#     room_id = data['roomId']
-#     username = data.get('username')  # Assuming you get a username too
-
-#     # Create the room if it doesn't exist
-#     if room_id not in game_rooms:
-#         game_rooms[room_id] = {
-#             'players': [], 
-#             'moves': {}
-#         }
-
-#     # Add the player to the room  
-#     game_rooms[room_id]['players'].append({'id': socketio.sid, 'username': username})
-
-#     join_room(room_id)  # Associate the player's SocketIO session with the room
+  # Associate the player's SocketIO session with the room
 
 #     # Check if room is full (limit of 2 players)
 #     if len(game_rooms[room_id]['players']) == 2:
